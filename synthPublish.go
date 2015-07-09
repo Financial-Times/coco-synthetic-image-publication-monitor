44b443fa9794131a9a8b3d4b62abaace6556e3fc
Zoltan Bodor <Zoltan.Bodor@ft.com>
2015-07-09 07:38:44 +0300

    go fmt

--- synthPublish.go
+++ synthPublish.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/json"
-        "flag"
+	"flag"
 	"fmt"
 	"net/http"
 	"time"
@@ -10,40 +10,41 @@ import (
 
 type synthPublApp struct {
 	postEndpoint string
-	s3          string
-        uuid        string
+	s3           string
+	uuid         string
 }
 
 type publication struct {
-    succeeded   bool
-    errorMsg    string
+	succeeded bool
+	errorMsg  string
 }
 
-var postEndpoint = flag.String("postEndpoint","cms-notifier-pr-uk-int.svc.ft.com","publish endpoint address (most probably the address of cms-notifier in UCS)")
+var postEndpoint = flag.String("postEndpoint", "cms-notifier-pr-uk-int.svc.ft.com", "publish endpoint address (most probably the address of cms-notifier in UCS)")
 var tick = flag.Bool("tick", true, "true, if this service should periodially generate and post content to the post endpoint")
+
 //fixed
 var uuid = "01234567-89ab-cdef-0123-456789abcdef"
 
 func main() {
 	fmt.Printf("Starting synthetic image publication monitor...")
 
-        flag.Parse()
-        app := &synthPublApp{
-            postEndpoint: *postEndpoint,
-            uuid: uuid,
-        }
-        var _ = app
+	flag.Parse()
+	app := &synthPublApp{
+		postEndpoint: *postEndpoint,
+		uuid:         uuid,
+	}
+	var _ = app
 
-        bytes := make(chan []byte)
-        lastResult := make(chan publication)
-        if *tick {
-            ticker := time.NewTicker(time.Second)
-            go func() {
-                    for _ = range ticker.C {
-                        app.publish(bytes,lastResult)
-                    }
-            }()
-        }
+	bytes := make(chan []byte)
+	lastResult := make(chan publication)
+	if *tick {
+		ticker := time.NewTicker(time.Second)
+		go func() {
+			for _ = range ticker.C {
+				app.publish(bytes, lastResult)
+			}
+		}()
+	}
 	http.HandleFunc("/__health", func(w http.ResponseWriter, r *http.Request) { fmt.Fprintf(w, "Healthcheck endpoint") })
 	http.HandleFunc("/forcePublish", func(w http.ResponseWriter, r *http.Request) { fmt.Fprintf(w, "force publish") })
 	http.HandleFunc("/test", testHandler)

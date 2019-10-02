495506467e09b9e3d00b1a8f6b5e72e9654685d5
Donislav Belev <donislav.belev@ft.com>
2019-10-02 18:04:44 +0300

    Adding kubectl authentication

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 sed -i "s@JENKINS_TOKEN@$JENKINS_TOKEN@g" /root/.kube/config
+sed -i "s/ENVNAME/$ENVNAME/g" /root/.kube/config
 /coco-synthetic-image-publication-monitor -postHost=$POST_ADDRESS -postCredentials="$POST_CREDENTIALS" -s3Host=$BUCKET_ADDRESS.$AWS_ADDRESS -testUuid=$TEST_UUID

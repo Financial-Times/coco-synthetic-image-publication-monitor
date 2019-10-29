9e90c1a7d60d94a5f739b78710ce00889e70d101
Donislav Belev <donislav.belev@ft.com>
2019-10-29 17:08:43 +0200

    Triggering alarm

--- synth-publish.go
+++ synth-publish.go
@@ -218,8 +218,8 @@ func checkPublishingStatus(latest postedData, result chan<- publicationResult, s
 		return
 	}
 	defer resp.Body.Close()
-	statusCode := http.StatusNotFound //resp.StatusCode
-	switch resp.StatusCode {
+	StatusCode := http.StatusNotFound //resp.StatusCode
+	switch StatusCode {
 	case http.StatusOK:
 		cmdR := exec.Command("kubectl", "delete", "cm", "synthetic-image-alarm", "--ignore-not-found")
 		errR := cmdR.Run()
